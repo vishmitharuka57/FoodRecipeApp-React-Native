c7487f135c7b20ce3f813032838e19e5ebc1b8b4
vishmitharuka57 <tharukavishmipk24@gmail.com>
2024-08-03 21:30:09 +0530

    initialize project and make screens and navigations

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export default function WelcomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center space-y-10 bg-red-500">
+      <Text>WelcomeScreen</Text>
+    </View>
+  );
+}
